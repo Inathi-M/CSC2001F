8f5c488b00d7a922dbfe4e4a90af98e95ed07205
Inathi-M <tshaphilenkosnathi@gmail.com>
2022-03-21 00:52:19 +0200

    Updated the Makefile and finally managed to make it work in the end.

---
+++ Makefile
@@ -0,0 +1,24 @@
+#Makefile for Assignment 1
+#Nkosinathi Tshaphile
+#28 February 2022
+
+JAVAC=/usr/bin/javac
+.SUFFIXES: .java .class
+SRCDIR=src
+BINDIR=bin
+
+$(BINDIR)/%.class:$(SRCDIR)/%.java
+	$(JAVAC) -d $(BINDIR)/ -cp $(BINDIR) $<
+	
+CLASSES=BinaryTreeNode.class BTQueueNode.class   BTQueue.class  BinaryTree.class AVLTree.class AVLExperiment.class  
+CLASS_FILES=$(CLASSES:%.class=$(BINDIR)/%.class)
+
+default: $(CLASS_FILES)
+         
+
+
+clean: 
+	rm $(BINDIR)/*.class
+	
+run: $(CLASS_FILES)
+	java -cp $(BINDIR) AVLExperiment  
--- src/AVLExperiment.java
+++ src/AVLExperiment.java
@@ -8,17 +8,36 @@ public class AVLExperiment {
 	static String[] arrDate = new String[9919];
 	static String[] arrnoOfVaccines = new String[9919];
    public static void main(String[] args) throws IOException {
-	   Scanner file = new Scanner(new File("C:/UCT/Assignment 2/vaccinacions.cvs"));
+	   
+	   Scanner file = new Scanner(new File("C:/UCT/Assignment 2/vaccinations.csv"));
 	   
 	   file.nextLine();
 	   
-	   
+	   int counter = 0;
 	   while(file.hasNextLine()) {
 		   String line[] = file.nextLine().split(",",-1);
 		   
-		   arrCountry
+		   String country = line[0];
+		   String date = line[1];
+		   String noOfVaccine = line[2];
 		   
+		   
+		   arrCountry[counter] = country;
+		   arrDate[counter] = date;
+		   arrnoOfVaccines[counter] = noOfVaccine;
+		   
+		   
+		  
+		   counter++;
 	   }
+	   
+	   for (int a = 0;a < 9919;a++)
+	   {
+		   
+		   System.out.println(arrCountry[a]);
+	   }
+	   
+	   
 	   
    }
 }
