0fb52ae393abcfee8cea791667cf12f1febcf871
Inathi-M <tshaphilenkosnathi@gmail.com>
2022-03-21 15:38:23 +0200

    Introduced two new methods on the AVLExperiment class

--- src/AVLExperiment.java
+++ src/AVLExperiment.java
@@ -4,10 +4,102 @@ import java.io.*;
 
 
 public class AVLExperiment {
+	AVLTree<String> avl = new AVLTree<String> ();
 	static String[] arrCountry = new String[9919]; 
 	static String[] arrDate = new String[9919];
 	static String[] arrnoOfVaccines = new String[9919];
+	public static BinaryTree btree = new BinaryTree();
+	public static AVLTree first_avl = new AVLTree();
+	public static AVLTree second_avl = new AVLTree();
+	public static AVLTree<String> aCountry;
+	public static AVLTree<String>  avl_tree;
+	
+	
+	 /*
+	  This method is used to determine the number of iterations it takes for the traditional array to find the given ]
+	  combinations of country + date.
+	  The result is the iterations for each country and the country name.s   
+	*/
+	      public static String printComparisons(String place, String day,BinaryTreeNode<String> rootNode,int SampleSize) throws NullPointerException{
+	      int averagecase = 0;
+	      String result = "";
+	      BinaryTreeNode<String> BTN = aCountry.find(place,rootNode);
+	      if (BTN == null){
+	            result = place + " = <Not Found>";
+	      }
+	      
+	      boolean check = false;
+	      try{
+	      String data = BTN.getData();
+	      bestcase = bstree.OperationsCount();
+	      for(int a = 0;a < 9918;a++){
+	         if((arrCountry[a] + arrDate[a]).equals(data + day)){
+	             check = true;
+	             result = ("There are  "+bstree.OperationsCount() + " iterations for "+ arrCountry[a] );
+	                  if(bestcase > bstree.OperationsCount())
+	                   bestcase = bstree.OperationsCount(); 
+	                  
+	                  if(worstcase < bstree.OperationsCount())
+	                   worstcase = bstree.OperationsCount();
+	         }
+	      }
+	      
+	      if(check == false){
+	         result = (place + " = <Not Found>");
+	      }
+	      return result;
+	      
+	      }catch(Exception e){
+	            return result;
+	      }
+	   }
+
+	/*
+	   This method is used to test the traditional array using 10 different subsets of values that are spaced equally apart.
+	   For each subset of values the number of iterations required to find the country + date combination is given
+	   The best case, the worst case and the aveage case is determined for each seperate subset
+	   Each result is written to a separate textfile ranging from sample1 to sample10 for the 10 substet values.
+	   The method also takes in the subset value, the Date and the PrintWriter file as arguments.
+	**/      
+	      public static String calculatesubsets(int subset,String sDate,PrintWriter sampledata){  
+	         Random randNum=new Random();        
+	         int opCount = 0;
+	         int average = 0;   
+	         int smallest = country.OperationsCount();
+	         int biggest = country.OperationsCount();
+	         int  max=subset;
+	         int    min=1;
+	         int  n_subset=min+ randNum.nextInt(max);
+	         for(int p=0;p<subset -1;p++){
+	         		  int l=n_subset;  
+	           if (l+p > 9917){
+	            System.out.println("Number of lines exceeded");
+	            break;
+	           }else{    
+	              System.out.println(printComparisons(arrT[l+p],sDate,country.root,subset));
+	              
+	              opCount = opCount + country.OperationsCount();
+	        	     sampledata.println(printComparisons(arrT[l+p],sDate,country.root,subset));
+	              if (smallest > country.OperationsCount()){
+	                  smallest = country.OperationsCount();
+	              } //end of if
+	              if(biggest < country.OperationsCount()){
+	                  biggest = country.OperationsCount();
+	              }
+					 }
+	            } 
+	             
+	             average  = (opCount / subset);
+	             System.out.println(average);
+	             System.out.println("The average case is" + average +"|The worst case is "+biggest + "|And the best case is "+ smallest) ; 
+	             sampledata.println("The average case is" + average +"|The worst case is "+biggest + "|And the best case is "+ smallest); 
+	            return ("The average case " + average +" " +opCount +" "+ subset);
+	   }	
+	
+	
+	
    public static void main(String[] args) throws IOException {
+	   aCountry = new AVLTree<String>();
 	   
 	   Scanner file = new Scanner(new File("C:/UCT/Assignment 2/vaccinations.csv"));
 	   
@@ -25,19 +117,21 @@ public class AVLExperiment {
 		   arrCountry[counter] = country;
 		   arrDate[counter] = date;
 		   arrnoOfVaccines[counter] = noOfVaccine;
-		   
-		   
-		  
 		   counter++;
+		   
+		   String allData = country + date + noOfVaccine;
+		   
+		   //String justCountry = country;
+		   
+		   aCountry.insert(country);
+		   second_avl.insert(country);
 	   }
 	   
 	   for (int a = 0;a < 9919;a++)
 	   {
 		   
 		   System.out.println(arrCountry[a]);
-	   }
-	   
-	   
+	   }   
 	   
    }
 }
